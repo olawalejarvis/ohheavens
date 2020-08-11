f467f4e311e95c9a339f42d6a10b64f32a1ed3b8
Olawale Aladeusi <olawalejarvis@gmail.com>
2020-08-11 12:34:50 +0100

    setup express server

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 9080;
 
 app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/app-ads.txt', (req, res) => {
+  res.sendFile(path.join(__dirname, './', 'app-ads.txt'));
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/', 'index.html'));
 });
